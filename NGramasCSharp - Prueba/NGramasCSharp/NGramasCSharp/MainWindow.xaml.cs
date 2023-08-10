8484042c59033d22af6f6114304f639d49f1eaf7
Daniel <germancito.sanz@gmail.com>
2023-08-10 00:50:51 -0400

    Añadiendo la prueba de cadenas de Markov

---
+++ NGramasCSharp - Prueba/NGramasCSharp/NGramasCSharp/MainWindow.xaml.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NGramasCSharp
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string pathCorpusFileMusic = "D:\\Prueba\\NGramasCSharp\\corpus.txt";
+        string corpusFile;
+        string pathCorpusFile = "D:\\Prueba\\NGramasCSharp\\corpus.txt";
+
+        Regex regex;
+        MarkovChain markovChain = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            markovChain = new MarkovChain();
+            corpusFile = InitializeCorpusFile();            
+        }
+
+        private void DisplayTransitionMatrix()
+        {
+            lbViewNGramas.Items.Clear();
+
+            foreach (var pair in markovChain.TransitionMatrix)
+            {
+                string word = pair.Key;
+                List<Transition> transitions = pair.Value;
+
+                string entry = $"\"{word}\":";
+                foreach (var transition in transitions)
+                {
+                    entry += $" {transition.Key} ({transition.Concurrency} / {transition.Probability:F2}),";
+                }
+
+                lbViewNGramas.Items.Add(entry.TrimEnd(','));
+            }
+        }
+
+        private string InitializeCorpusFile()
+        {
+            try
+            {
+                if (!File.Exists(pathCorpusFile))
+                    return "";
+
+                StringBuilder sb = new StringBuilder();
+                using (StreamReader sr = new StreamReader(pathCorpusFile, Encoding.Default))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        sb.AppendLine(line);
+                        markovChain.AddTransition(line);
+                        markovChain.NormalizeProbabilities();
+                        if (sb.Length > 10000)
+                            break;
+                    }
+                    string c = sb.ToString();
+                    DisplayTransitionMatrix();
+                    return c;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al leer el archivo: " + ex.Message);
+                return "";
+            }
+        }
+
+        private string GetLastWord(string word)
+        {
+            if (!word.Contains(' '))
+                return word;
+
+            string isSpace = word.Substring(word.LastIndexOf(' ') + 1);
+            regex = new Regex(@"[^\w\d]+");
+            isSpace = regex.Replace(isSpace, string.Empty);
+
+            //MessageBox.Show($"\"{isSpace}\"");
+            return isSpace.ToLower();
+        }
+
+        private void txtInput_KeyUp(object sender, KeyEventArgs e)
+        {
+            string lastWord = GetLastWord(txtInput.Text);
+
+            lbWordsPredict.Items.Clear();
+                        
+            var predicts = markovChain.GetNextWords(lastWord, int.Parse(txtLength.Text));
+            foreach (string pre in predicts)
+            {
+                lbWordsPredict.Items.Add(pre);
+            }
+            
+            //lbWordsPredict.Items.Add(markovChain.GenerateSentence(txtInput.Text, int.Parse(txtLength.Text), int.Parse(txtEpochs.Text)));
+        }
+
+        private void btnOk_Click(object sender, RoutedEventArgs e)
+        {
+            using (StreamWriter sw = new StreamWriter(pathCorpusFile))
+            {
+                sw.Write(corpusFile);
+                sw.WriteLine(txtInput.Text.ToLower());
+            }
+
+            // InitializeCorpusFile();
+            // MessageBox.Show(corpusFile);
+
+            markovChain.AddTransition(txtInput.Text);
+            markovChain.NormalizeProbabilities();
+            DisplayTransitionMatrix();
+        }                
+
+        #region NGramas por carácter
+        private Dictionary<string, List<string>> BuildNGramasByChar(int numChar, string corpus)
+        {
+            Dictionary<string, List<string>> dic = new Dictionary<string, List<string>>();
+
+                for (int i = 0; i < corpus.Length - numChar; i++)
+                {
+                    string secuencie = corpus.Substring(i, numChar);
+                    if (!dic.ContainsKey(secuencie))
+                        dic[secuencie] = new List<string>();
+                    dic[secuencie].Add(corpus[i + numChar].ToString());
+                }
+
+                return dic;            
+        }        
+
+        //private string GetPredicts(string frase, int numChar, int epochs)
+        //{
+        //    string res = frase;
+        //    Random rnd = new Random();
+
+        //    for (int i = 0; i < epochs; i++)
+        //    {
+        //        if (!dicNGramas.ContainsKey(frase))
+        //            break;
+        //        List<string> car_posibles = dicNGramas[frase];
+        //        string sig_car = car_posibles[rnd.Next(car_posibles.Count())];
+        //        res += sig_car;
+        //        frase = res.Substring(res.Length - numChar);
+        //    }
+
+        //    return res;
+        //}
+        #endregion
+
+        #region NGramas por palabras        
+        //private Dictionary<string, List<string>> BuildNGramasByWord(int words, string corpus)
+        //{
+        //    Dictionary<string, List<string>> dic = new Dictionary<string, List<string>>();
+        //    string[] tokens = Tokenization(corpus);
+
+        //    for (int i = 0; i < tokens.Length-words; i++)
+        //    {
+        //        string secuence = "";
+        //        for (int j = i; j < i+words; j++)
+        //        {
+        //            secuence += tokens[j] + " ";
+        //        }   
+        //        if (!dic.ContainsKey(secuence))
+        //            dic[secuence] = new List<string>();
+        //        dic[secuence].Add(tokens[i+words]);
+        //    }
+
+        //    return dic;
+        //}
+
+        //private string GetPredictsByWords(string frase, int num, int epochs)
+        //{
+        //    string res = frase;
+        //    Random rnd = new Random();
+
+        //    for (int i = 0; i < epochs; i++)
+        //    {
+        //        if (!dicNGramasWord.ContainsKey(frase))
+        //            break;
+        //        List<string> car_posibles = dicNGramasWord[frase];
+        //        string sig_car = car_posibles[rnd.Next(car_posibles.Count())];
+        //        res += " " + sig_car;
+        //        string[] tokens = Tokenization(res);
+        //        string secuence = "";
+        //        for (int j = i; j < i + num; j++)
+        //        {
+        //            secuence += tokens[j] + " ";
+        //        }
+        //        frase = res.Substring(secuence.Length - num);
+        //    }
+
+        //    return res;
+        //}
+        #endregion
+    }
+}
---
+++ NGramasCSharp - Prueba/NGramasCSharp/NGramasCSharp/MarkovChain.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace NGramasCSharp
+{
+    public class MarkovChain
+    {
+        private Dictionary<string, List<Transition>> transitionMatrix;
+        private char[] delimiters;
+        private Random rnd;
+
+        public Dictionary<string, List<Transition>> TransitionMatrix
+        {
+            get { return transitionMatrix; }
+        }
+
+        public MarkovChain()
+        {
+            transitionMatrix = new Dictionary<string, List<Transition>>();
+            delimiters = new char[] { ' ', '.', ',', '!', '?', '\n', '\r' };
+            rnd = new Random();
+        }
+
+        private string[] Tokenization(string sentence)
+        {
+            string[] tokens = sentence.ToLower().Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+            return tokens;
+        }
+
+        public void AddTransition(string sentence)
+        {
+            string[] tokens = Tokenization(sentence);
+
+            for (int i = 0; i < tokens.Length - 1; i++)
+            {
+                string currentWord = tokens[i];
+                string nextWord = tokens[i + 1];
+
+                if (!transitionMatrix.ContainsKey(currentWord))
+                    transitionMatrix[currentWord] = new List<Transition>();
+                
+                Transition currentTransition = transitionMatrix[currentWord].FirstOrDefault(t => t.Key == nextWord);
+                if (currentTransition == null)
+                    transitionMatrix[currentWord].Add(new Transition(1, nextWord));                                       
+                else                
+                    currentTransition.Concurrency++;                                                     
+            }
+        }
+
+        public void NormalizeProbabilities()
+        {
+            foreach (var chain in transitionMatrix)
+            {
+                double totalTransitions = chain.Value.Sum(t => t.Concurrency);
+                foreach (var transition in chain.Value)
+                {
+                    transition.Probability = transition.Concurrency / totalTransitions;
+                }
+            }
+        }        
+
+        public List<string> GetNextWords(string currentWord, int numWords)
+        {
+            List<string> result = new List<string>();
+
+            if(!transitionMatrix.ContainsKey(currentWord))
+                return result;
+
+            var transitions = transitionMatrix[currentWord];
+            var sortedTransitions = transitions.OrderByDescending(pair => pair.Probability);
+
+            foreach (var pair in sortedTransitions)
+            {
+                string nextWord = pair.Key;
+                if (nextWord != currentWord)
+                    result.Add(nextWord);
+
+                if (result.Count() >= numWords)
+                    break;
+            }
+
+            return result;
+        }
+
+        //public string GenerateSentence(string startingWord, int maxLength, int epochs)
+        //{
+        //    if (!transitionMatrix.ContainsKey(startingWord))
+        //        return startingWord;
+
+        //    StringBuilder result = new StringBuilder();
+
+        //    for (int epoch = 0; epoch < epochs; epoch++)
+        //    {
+        //        string currentWord = startingWord;
+        //        List<string> sentence = new List<string> { currentWord };
+
+        //        while (transitionMatrix.ContainsKey(currentWord) && sentence.Count < maxLength)
+        //        {
+        //            double diceRoll = rnd.NextDouble();
+        //            double cumulativeProbability = 0;
+
+        //            foreach (var pair in transitionMatrix[currentWord])
+        //            {
+        //                cumulativeProbability += pair.Value;
+        //                if (diceRoll < cumulativeProbability)
+        //                {
+        //                    currentWord = pair.Key;
+        //                    sentence.Add(currentWord);
+        //                    break;
+        //                }
+        //            }
+        //        }
+
+        //        result.AppendLine(string.Join(" ", sentence));
+        //    }
+
+        //    return result.ToString();
+        //}
+    }
+}
